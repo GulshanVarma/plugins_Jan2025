771b6d7af7a7f87f9ee19b6b793a11d032288a2b
Gulshan Varma <varmagulshan11@gmail.com>
2025-01-09 23:27:50 +0530

    check user score - related

--- PluginsJan2025/prevalidationPlugin.cs
+++ PluginsJan2025/prevalidationPlugin.cs
@@ -22,8 +22,35 @@ namespace PluginsJan2025
                     IOrganizationServiceFactory orgFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                     IOrganizationService service = orgFactory.CreateOrganizationService(context.UserId);
 
-                    if (context.MessageName.ToLower() == "create" || context.Stage == 20)
+                    if (context.MessageName.ToLower() == "create" || context.Stage == 10)
                     {
+                        Entity TargetEntity = context.InputParameters["Target"] as Entity;
+                        if (string.IsNullOrEmpty(TargetEntity.GetAttributeValue<EntityReference>("gv_pan")?.Id.ToString()))
+                        {
+                            throw new Exception("PAN is not selected");
+                        }
+
+                        // check valid score from Loan > Account > User
+
+                        /*QueryExpression queryExpression = new QueryExpression() { 
+                            EntityName = "gv_Bank_Account",
+                            ColumnSet = new ColumnSet("gv_accountnumber", "gv_AccountHolder")
+                        };*/
+
+
+                        Entity AccountDetails = service.Retrieve("gv_bank_account", TargetEntity.GetAttributeValue<EntityReference>("gv_pan").Id, 
+                            new ColumnSet("gv_accountnumber", "gv_accountholder"));
+
+                        Entity userDetail = service.Retrieve("gv_bank_user", AccountDetails.GetAttributeValue<EntityReference>("gv_accountholder").Id,
+                            new ColumnSet("gv_userscore"));
+
+                        Entity LoanConfigDetails = service.Retrieve("gv_bank_loan_config", TargetEntity.GetAttributeValue<EntityReference>("gv_loantype").Id,
+                            new ColumnSet("gv_minimumscore"));
+
+                        if(LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore") > userDetail.GetAttributeValue<Int32>("gv_userscore"))
+                        {
+                            throw new Exception("user score is lower than the required limit - "+ LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore"));
+                        }
 
                     }
                     else
@@ -39,4 +66,5 @@ namespace PluginsJan2025
                 tracingService.Trace($"Exception Caught - {ex.Message}, trace -- {ex.StackTrace}");
             }
         }
+    }
 }
