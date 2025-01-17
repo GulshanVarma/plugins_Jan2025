c74fbcaa91f48294dd6b5d3eac9b936036b103c0
Gulshan Varma <varmagulshan11@gmail.com>
2025-01-17 11:33:54 +0530

    update

---
+++ PluginsJan2025/Plugin_Loans.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace PluginsJan2025
+{
+    public class plg_OnCreate_Loans_PreOps : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            try
+            {
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    IOrganizationServiceFactory orgFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = orgFactory.CreateOrganizationService(context.UserId);
+
+                    if (context.MessageName.ToLower() == "create" || context.Stage == 20)
+                    {
+                        tracingService.Trace("plg_OnCreate_Loans_PreOps -> Create Message, Pre - Operation Plugin");
+
+                        Entity TargetEntity = context.InputParameters["Target"] as Entity;
+
+                        // lookup is entity reference, not guid. Get Lookup details
+                        Guid LoanTypeGuid = TargetEntity.GetAttributeValue<EntityReference>("gv_loantype").Id;
+                        Entity LoanTypeEntity = service.Retrieve("gv_bank_loan_config", LoanTypeGuid, new ColumnSet("gv_bank_loan_configid", "gv_interestrate", "gv_loantenure"));
+                        tracingService.Trace("plg_OnCreate_Loans_PreOps -> Fetched Lookup details successfully");
+
+                        // fill empty values from Target Entity -> (null values on form are not included in entity context)
+                        if (TargetEntity.Contains("gv_loantenure"))
+                            TargetEntity["gv_loantenure"] = LoanTypeEntity.GetAttributeValue<Int32>("gv_loantenure");
+                        TargetEntity.Attributes.Add("gv_loantenure", LoanTypeEntity.GetAttributeValue<Int32>("gv_loantenure"));
+
+                        if (TargetEntity.Contains("gv_interest"))
+                            TargetEntity["gv_interest"] = LoanTypeEntity.GetAttributeValue<decimal>("gv_interestrate");
+                        TargetEntity.Attributes.Add("gv_interest", LoanTypeEntity.GetAttributeValue<decimal>("gv_interestrate"));
+                        tracingService.Trace("plg_OnCreate_Loans_PreOps -> Autofilled the records");
+                    }
+                    else
+                    {
+                        tracingService.Trace($"plg_OnCreate_Loans_PreOps -> Invalid Plugin Context, cant proceeding further. message - {context.MessageName.ToLower()}, stage - {context.Stage}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace($"plg_OnCreate_Loans_PreOps -> Exception Caught - {ex.Message}, trace -- {ex.StackTrace}");
+            }
+        }
+    }
+
+    public class plg_OnUpdate_Loans_PostOps : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            try
+            {
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    IOrganizationServiceFactory orgFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = orgFactory.CreateOrganizationService(context.UserId);
+
+                    Entity Loandetails = null,AccountDetails = null,userDetail = null,LoanConfigDetails = null;
+                    Entity TargetEntity = context.InputParameters["Target"] as Entity;
+                    
+                    // we get only ID and updated field
+                    if (context.MessageName.ToLower() == "update" && context.Stage == 40)
+                    {
+                        // load additional fields of Target Entity
+                        ColumnSet getDataForLoan = new ColumnSet("gv_loantype", "gv_pan");
+                        Loandetails = service.Retrieve("gv_bank_loan", TargetEntity.Id, getDataForLoan);
+
+                        // load pan details 
+                        AccountDetails = service.Retrieve("gv_bank_account", Loandetails.GetAttributeValue<EntityReference>("gv_pan").Id,
+                            new ColumnSet("gv_accountnumber", "gv_accountholder"));
+
+                        // load user details - userScore
+                        userDetail = service.Retrieve("gv_bank_user", AccountDetails.GetAttributeValue<EntityReference>("gv_accountholder").Id,
+                            new ColumnSet("gv_userscore", "gv_userfullname"));
+
+                        // get LoanConfig data - required score
+                        LoanConfigDetails = service.Retrieve("gv_bank_loan_config", Loandetails.GetAttributeValue<EntityReference>("gv_loantype").Id,
+                            new ColumnSet("gv_minimumscore"));
+
+                        if (LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore") > userDetail.GetAttributeValue<Int32>("gv_userscore"))
+                        {
+                            tracingService.Trace($"plg_OnUpdate_Loans_PostOps -> User {userDetail.GetAttributeValue<string>("gv_userfullname")} -> score {userDetail.GetAttributeValue<Int32>("gv_userscore")} is lower than the required limit - {LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore")}");
+                            throw new InvalidPluginExecutionException("user score is lower than the required limit - " + LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore"));
+                        }
+                        tracingService.Trace($"plg_OnUpdate_Loans_PostOps -> Update Success. User {userDetail.GetAttributeValue<string>("gv_userfullname")} -> score {userDetail.GetAttributeValue<Int32>("gv_userscore")} is greater than the required limit - {LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore")}");
+                    }
+                }
+                else
+                {
+                    tracingService.Trace($"plg_OnUpdate_Loans_PostOps -> Invalid Plugin Context, cant proceeding further. message - {context.MessageName.ToLower()}, stage - {context.Stage}");
+                }
+            }
+            catch (InvalidPluginExecutionException) { throw; }
+            catch (Exception ex)
+            {
+                tracingService.Trace($"Exception Caught -> Msg - {ex.Message}, trace - {ex.StackTrace}");
+            }
+        }
+    }
+}
--- PluginsJan2025/prevalidationPlugin.cs
+++ PluginsJan2025/prevalidationPlugin.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PluginsJan2025
 {
@@ -13,43 +9,60 @@ namespace PluginsJan2025
         public void Execute(IServiceProvider serviceProvider)
         {
             ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             try
             {
-                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-
                 if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
                 {
                     IOrganizationServiceFactory orgFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                     IOrganizationService service = orgFactory.CreateOrganizationService(context.UserId);
 
-                    if (context.MessageName.ToLower() == "create" || context.Stage == 10)
+                    Entity Loandetails = null;
+                    Entity AccountDetails = null;
+                    Entity userDetail = null;
+                    Entity LoanConfigDetails = null;
+
+                    if ((context.MessageName.ToLower() == "create" && context.Stage == 10) || (context.MessageName.ToLower() == "update" && context.Stage == 10))
                     {
+                        tracingService.Trace($" Creation/Update Plugin, prevalidation started");
                         Entity TargetEntity = context.InputParameters["Target"] as Entity;
-                        if (string.IsNullOrEmpty(TargetEntity.GetAttributeValue<EntityReference>("gv_pan")?.Id.ToString()))
+                        if (context.MessageName.ToLower() == "create" && string.IsNullOrEmpty(TargetEntity.GetAttributeValue<EntityReference>("gv_pan")?.Id.ToString()))
                         {
-                            throw new Exception("PAN is not selected");
+                            tracingService.Trace($"Pan is not selected");
+                            return;
                         }
 
-                        // check valid score from Loan > Account > User
-
-                        /*QueryExpression queryExpression = new QueryExpression() { 
-                            EntityName = "gv_Bank_Account",
-                            ColumnSet = new ColumnSet("gv_accountnumber", "gv_AccountHolder")
-                        };*/
-
-
-                        Entity AccountDetails = service.Retrieve("gv_bank_account", TargetEntity.GetAttributeValue<EntityReference>("gv_pan").Id, 
-                            new ColumnSet("gv_accountnumber", "gv_accountholder"));
-
-                        Entity userDetail = service.Retrieve("gv_bank_user", AccountDetails.GetAttributeValue<EntityReference>("gv_accountholder").Id,
-                            new ColumnSet("gv_userscore"));
-
-                        Entity LoanConfigDetails = service.Retrieve("gv_bank_loan_config", TargetEntity.GetAttributeValue<EntityReference>("gv_loantype").Id,
-                            new ColumnSet("gv_minimumscore"));
-
-                        if(LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore") > userDetail.GetAttributeValue<Int32>("gv_userscore"))
+                        if ((context.MessageName.ToLower() == "update" && context.Stage == 10))
                         {
-                            throw new Exception("user score is lower than the required limit - "+ LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore"));
+                            // fetch remainig attributes, we get only ID and updated field
+                            ColumnSet getDataForLoan = new ColumnSet("gv_loantype","gv_pan");
+                            Loandetails = service.Retrieve("gv_bank_loan", TargetEntity.Id, getDataForLoan);
+                            AccountDetails = service.Retrieve("gv_bank_account", Loandetails.GetAttributeValue<EntityReference>("gv_pan").Id,
+                                new ColumnSet("gv_accountnumber", "gv_accountholder"));
+
+                            userDetail = service.Retrieve("gv_bank_user", AccountDetails.GetAttributeValue<EntityReference>("gv_accountholder").Id,
+                                new ColumnSet("gv_userscore", "gv_userfullname"));
+
+                            LoanConfigDetails = service.Retrieve("gv_bank_loan_config", Loandetails.GetAttributeValue<EntityReference>("gv_loantype").Id,
+                                new ColumnSet("gv_minimumscore"));
+                        }
+                        else
+                        {
+                            AccountDetails = service.Retrieve("gv_bank_account", TargetEntity.GetAttributeValue<EntityReference>("gv_pan").Id,
+                                new ColumnSet("gv_accountnumber", "gv_accountholder"));
+
+                            userDetail = service.Retrieve("gv_bank_user", AccountDetails.GetAttributeValue<EntityReference>("gv_accountholder").Id,
+                                new ColumnSet("gv_userscore", "gv_userfullname"));
+
+                            LoanConfigDetails = service.Retrieve("gv_bank_loan_config", TargetEntity.GetAttributeValue<EntityReference>("gv_loantype").Id,
+                                new ColumnSet("gv_minimumscore"));
+                        }
+
+                        if (LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore") > userDetail.GetAttributeValue<Int32>("gv_userscore"))
+                        {
+                            tracingService.Trace($"user {userDetail.GetAttributeValue<string>("gv_userfullname")} -> score {userDetail.GetAttributeValue<Int32>("gv_userscore")} is lower than the required limit - {LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore")}");
+                            throw new InvalidPluginExecutionException("user score is lower than the required limit - " + LoanConfigDetails.GetAttributeValue<Int32>("gv_minimumscore"));
                         }
 
                     }
@@ -59,11 +72,11 @@ namespace PluginsJan2025
                     }
 
                 }
-
             }
+            catch (InvalidPluginExecutionException) { throw; }
             catch (Exception ex)
             {
-                tracingService.Trace($"Exception Caught - {ex.Message}, trace -- {ex.StackTrace}");
+                tracingService.Trace($"Exception Caught -> Msg - {ex.Message}, trace - {ex.StackTrace}");
             }
         }
     }
