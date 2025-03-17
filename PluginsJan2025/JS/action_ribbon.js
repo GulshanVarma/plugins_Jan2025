6b579c448a941dcc6e93663d3877c80f2d89ee42
Gulshan Varma <varmagulshan11@gmail.com>
2025-03-17 23:09:26 +0530

    external api code

--- PluginsJan2025/JS/action_ribbon.js
+++ PluginsJan2025/JS/action_ribbon.js
@@ -1,25 +1,111 @@
-// Parameters
-var parameters = {};
-parameters.inputmessage = "dummy"; // Edm.String
+if (BankingPortal == undefined){
+	var BankingPortal = {__namespace:true}
+}
+if (BankingPortal.gv_bank_loan == undefined) {
+	BankingPortal.gv_bank_loan = { __namespace: true }
+}
+if (BankingPortal.gv_bank_loan.Ribbon == undefined) {
+	BankingPortal.gv_bank_loan.Ribbon = { __namespace: true }
+}
 
-var req = new XMLHttpRequest();
-req.open("POST", Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.2/gv_bank_loans(137d1884-47d7-ef11-8eea-7c1e523d27d3)/Microsoft.Dynamics.CRM.gv_action_testSample", false);
-req.setRequestHeader("OData-MaxVersion", "4.0");
-req.setRequestHeader("OData-Version", "4.0");
-req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-req.setRequestHeader("Accept", "application/json");
-req.onreadystatechange = function () {
-	if (this.readyState === 4) {
-		req.onreadystatechange = null;
-		if (this.status === 200 || this.status === 204) {
-			var result = JSON.parse(this.response);
-			console.log(result);
-			// Return Type: mscrm.gv_action_testSampleResponse
-			// Output Parameters
-			var outputmessage = result["outputmessage"]; // Edm.String
-		} else {
-			console.log(this.responseText);
-		}
+
+BankingPortal.gv_bank_loan.Ribbon.Command = (function (primaryControl, primaryitemids, primaryentitytypename,
+	primaryentitytypecode, firstprimaryitemid, selectedentitytypecode, selectedentitytypename) {               // ()() = immediate invoking function
+
+	var createActionFunction = function (primaryControl, primaryitemids, primaryentitytypename,
+		primaryentitytypecode, firstprimaryitemid, selectedentitytypecode, selectedentitytypename) {
+		debugger
+		var formContext = primaryControl;
+		var inputParam = window.prompt("enter input: ");
+		// Parameters
+		var parameters = {};
+		parameters.inputmessage = inputParam; // Edm.String
+		var outputmessage = "";
+		var req = new XMLHttpRequest();
+		var guid = primaryControl._data._entity._entityId.guid;
+		req.open("POST", Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.2/gv_bank_loans(" + guid + ")/Microsoft.Dynamics.CRM.gv_action_testSample", false);
+		req.setRequestHeader("OData-MaxVersion", "4.0");
+		req.setRequestHeader("OData-Version", "4.0");
+		req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+		req.setRequestHeader("Accept", "application/json");
+		req.onreadystatechange = function () {
+			if (this.readyState === 4) {
+				req.onreadystatechange = null;
+				if (this.status === 200 || this.status === 204) {
+					var result = JSON.parse(this.response);
+					console.log(result);
+					// Return Type: mscrm.gv_action_testSampleResponse
+					// Output Parameters
+					 outputmessage = result["outputmessage"]; // Edm.String
+				} else {
+					console.log(this.responseText);
+				}
+			}
+		};
+		req.send(JSON.stringify(parameters));
+		Xrm.Utility.alertDialog(outputmessage);
 	}
-};
-req.send(JSON.stringify(parameters));
+
+	return{                                                               // return for ribbon button
+		var_CREATEActionFunction : createActionFunction
+	}
+})();   
+	
+// BankingPortal.gv_bank_loan.Ribbon.Command.var_CREATEActionFunction
+
+
+BankingPortal.gv_bank_loan.Ribbon.CommandGrid = (function (primaryControl,
+	selectedcontrolselecteditemids, selectedcontrolselecteditemrefernces, selectedcontrolallitemids, selectedcontrolselecteditemreferences) {               // ()() = immediate invoking function
+
+	var createActionFunction = function (primaryControl, selectedcontrolselecteditemids, selectedcontrolselecteditemrefernces, selectedcontrolallitemids, selectedcontrolselecteditemreferences) {
+		debugger
+		var formContext = primaryControl;
+		var inputParam = window.prompt("enter input: ");
+
+
+		var alertStrings = { confirmButtonLabel: "Yes", text: "This is an alert.", title: "Sample title" };
+		var alertOptions = { height: 120, width: 260 };
+		Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(
+			function (success) {
+				console.log("Alert dialog closed");
+			},
+			function (error) {
+				console.log(error.message);
+			}
+		);
+
+
+		// Parameters
+		var parameters = {};
+		parameters.inputmessage = inputParam; // Edm.String
+		var outputmessage = "";
+		var req = new XMLHttpRequest();
+		req.open("POST", Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.2/gv_bank_loans(137d1884-47d7-ef11-8eea-7c1e523d27d3)/Microsoft.Dynamics.CRM.gv_action_testSample", false);
+		req.setRequestHeader("OData-MaxVersion", "4.0");
+		req.setRequestHeader("OData-Version", "4.0");
+		req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+		req.setRequestHeader("Accept", "application/json");
+		req.onreadystatechange = function () {
+			if (this.readyState === 4) {
+				req.onreadystatechange = null;
+				if (this.status === 200 || this.status === 204) {
+					var result = JSON.parse(this.response);
+					console.log(result);
+					// Return Type: mscrm.gv_action_testSampleResponse
+					// Output Parameters
+					outputmessage = result["outputmessage"]; // Edm.String
+				} else {
+					console.log(this.responseText);
+				}
+			}
+		};
+		req.send(JSON.stringify(parameters));
+		Xrm.Utility.alertDialog(outputmessage);
+	}
+
+	return {                                                               // return for ribbon button
+		var_CREATEActionFunction: createActionFunction
+	}
+})();   
+
+// BankingPortal.gv_bank_loan.Ribbon.CommandGrid.var_CREATEActionFunction
---
+++ PluginsJan2025/SecurityRoles_ops.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginsJan2025
+{
+    public class SecurityRoles_ops : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.InitiatingUserId);
+
+            try
+            {
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    Entity InputEntity = (Entity)context.InputParameters["Target"];
+                    tracingService.Trace($"update plugin triggered for table - {InputEntity.LogicalName} and if column gv_name? - {InputEntity.Attributes.Contains("gv_name")}");
+                    if (InputEntity.LogicalName == "gv_project" && InputEntity.Attributes.Contains("gv_name"))
+                    {
+                        if (context.MessageName.ToLower() == "update")
+                        {
+                            tracingService.Trace($"update plugin triggered for table - {InputEntity.LogicalName} and column - {InputEntity["gv_name"]}");
+                           
+                            Guid userId = new Guid("513b8243-7bde-ef11-8eea-7c1e523d27d3");
+                            Guid roleIdRoles = new Guid("b6d85192-f9ea-ef11-be20-6045bdc5e2d2"); // tt_user role
+
+                            QueryExpression query = new QueryExpression("role");
+                            query.ColumnSet = new ColumnSet("name");
+
+                            LinkEntity userRoleLink = new LinkEntity("role", "systemuserrole", "roleid", "roleid", JoinOperator.Inner);
+                            //userRoleLink.LinkCriteria.AddCondition("roleid", ConditionOperator.Equal, roleIdRoles);
+
+                            //query.LinkEntities.Add(userRoleLink);
+
+                            EntityCollection results = service.RetrieveMultiple(query);
+
+                            foreach (Entity role in results.Entities)
+                            {
+                                string roleName = role.GetAttributeValue<string>("name");
+                                tracingService.Trace("Role: " + roleName);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                throw new InvalidPluginExecutionException("An error occurred in the plugin.", ex);
+            }
+            catch (Exception ex)
+            {
+                tracingService.Trace("CheckAndUpdateSecurityRole: {0}", ex.ToString());
+                throw;
+            }
+        }
+    }
+
+
+}
